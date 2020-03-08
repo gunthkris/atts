ed9bd5e693e1326e171893d5936c4e2d57d4a3cb
Gunther Santos <yamiluceid@gmail.com>
2020-03-07 21:22:02 -0500

    now has stereo

--- snapcam.py
+++ snapcam.py
@@ -1,15 +1,20 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
+camL = cv2.VideoCapture(0) # Cam L
+camR = cv2.VideoCapture(1) # Cam R
 
-cv2.namedWindow("test")
+
+cv2.namedWindow("Cam L")
+cv2.namedWindow("Cam R")
 
 img_counter = 0
 
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
+    retL, frameL = camL.read()
+    retR, frameR = camR.read()
+    cv2.imshow("Cam L", frameL)
+    cv2.imshow("Cam R", frameR)
+    if not retL or not retR:
         break
     k = cv2.waitKey(1)
 
@@ -19,9 +24,13 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
+        img_name = "opencv_frameL_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frameL)
         print("{} written!".format(img_name))
+        img_name = "opencv_frameR_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frameR)
+        print("{} written!".format(img_name))
+        
         img_counter += 1
 
 cam.release()
