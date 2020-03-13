414893b1ba36fe9052183b4e0a3ca61a19a7ea22
Gunther Santos <yamiluceid@gmail.com>
2020-03-12 21:03:54 -0400

    Stepper update

--- samples/panTiltControl.py
+++ samples/panTiltControl.py
@@ -3,30 +3,70 @@
 import RPi.GPIO as GPIO
 import time
 
-stepPin = 33
-dirPin = 35
-
 # set pin numbers to the board's
 GPIO.setmode(GPIO.BOARD)
+steps = 0.0005 # Speed of pulse in seconds
 
-GPIO.setup(stepPin, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(dirPin, GPIO.OUT, initial=GPIO.HIGH)
+class Stepper:
 
-print("Going forward")
-for i in range (200):
-    GPIO.output(stepPin, GPIO.HIGH)
-    time.sleep(0.0005)
-    GPIO.output(stepPin, GPIO.LOW)
-    time.sleep(0.0005)
+    microStep = 'FS'
+    stepperPos = 0
+    stepperSteps = 1.8 # Default at full step
+    stepperPosMin = -45.0 # 1.8 deg with full step (45 deg max)
+    stepperPosMax = 45.0 # same
+    steps = 0.0005
 
-print("Going backwards")
-# Go backwards
-GPIO.output(dirPin, GPIO.LOW)
+    # Define pins for step and direction
+    def __init__(self, stepPin, dirPin):
+        self.stepPin = stepPin
+        self.dirPin = dirPin
+        GPIO.setup(self.stepPin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.dirPin, GPIO.OUT, initial=GPIO.LOW)
 
-for i in range (400):
-    GPIO.output(stepPin, GPIO.HIGH)
-    time.sleep(0.0005)
-    GPIO.output(stepPin, GPIO.LOW)
-    time.sleep(0.0005)
+    def outOBounds:
+        if stepperPos <= stepperPosMin or stepperPos >= stepperPosMax
+            return true
+
+    def givePulse:
+        GPIO.output(stepPin, GPIO.HIGH)
+        time.sleep(steps)
+        GPIO.output(stepPin, GPIO.LOW)
+        time.sleep(steps)
+
+    def tiltFwd:
+        GPIO.output(dirPin, GPIO.LOW)
+        if outOfBounds():
+            break
+            print("Max forward angle")
+        else:
+            givePulse()
+            stepperPos -= stepperSteps
+
+    def tiltBwd:
+        GPIO.output(dirPin, GPIO.HIGH)
+        if outOfBounds():
+            break
+            print("Max backwards angle")
+        else:
+            givePulse()
+            stepperPos += stepperSteps
+
+tiltStepper = Stepper(19, 21)
+
+while (1):
+    print("Going forward")
+    for i in range(200):
+        tiltStepper.tiltFwd()
+        print(tiltStepper.stepperPos)
+
+    time.sleep(1)
+    print("Going backwards")
+    # Go backwards
+
+    for i in range(200):
+        tiltStepper.tiltBwd()
+        print(tiltStepper.stepperPos)
+
+    time.sleep(1)
 
 GPIO.cleanup()
