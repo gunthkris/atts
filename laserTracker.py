1997586e587e48cb57c284dea4ae2ef4b93b8aaf
Gunther Santos <yamiluceid@gmail.com>
2020-03-15 19:07:52 -0400

    target optimization

--- laserTracker.py
+++ laserTracker.py
@@ -155,7 +155,7 @@ while (True):
                0.65, (0, 0, 255), 1)
 
     # Move Turret based on location of target
-    if targetDetected:
+    if targetDetected and counter > 12:
         if lx > maxFrameWidth/2:
             ptc.pan.rotateCW()
         else:
