8a137ba81443e6426cc7a1d2b534543299f9b919
Gunther Santos <yamiluceid@gmail.com>
2020-04-18 15:05:24 -0400

    finalizing

--- laserTracker.py
+++ laserTracker.py
@@ -19,8 +19,8 @@ args = vars(ap.parse_args())
 # Laser Pointer
 # greenLower = (37, 9, 215)
 # greenUpper = (77, 77, 255)
-greenLower = (37, 9, 220)
-greenUpper = (55, 60, 255)
+greenLower = (45, 9, 220)
+greenUpper = (65, 60, 255)
 # Ball
 # greenLower = (40, 125, 70)
 # greenUpper = (70, 255, 170)
